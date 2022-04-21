55e450c07a02050c22f6882de966d027230fb13f
Kaz <takaik@yahoo.com>
2022-04-21 16:59:30 -0400

    whenever 途中, TimeWithZoneで書き換える

--- app/controllers/available_times_controller.rb
+++ app/controllers/available_times_controller.rb
@@ -1,7 +1,25 @@
 class AvailableTimesController < ApplicationController
+  require 'date'
 
   def index
     times = AvailableTime.all
     render json: times.as_json
   end
+
+  def add_time
+    Time.zone = 'Eastern Time (US & Canada)'
+    t = Time.zone.now + 30.day
+    date = "#{t.year}"
+    16.times do
+      AvailableTime.create!(date: , time: day.
+    end
+  end
 end
+
+4・21 ここまで
+
+タイムゾーンと90日後の日付を取得してから30分毎のレコードを自動作成
+whenever
+railsの TimeWithZoneを使う
+
+10:00 - 18:00
