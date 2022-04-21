ab040a7ec86ec61998b9bbb4e6c32b34e79b1d34
Kaz <takaik@yahoo.com>
2022-04-21 14:33:26 -0400

    install CORS & seed database for AvailableTimes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'date'
+
+AvailableTime.create!(date: Date.today.to_s, time: Time.now.strftime('%H:00'), available: true, event_id: 1)
